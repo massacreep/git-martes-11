cc17c4710ae962040801ae1aeac3a1ec10d33672
santi <massacreep@gmail.com>
2021-08-25 13:47:34 -0300

    Logica: Menu Hamburguesa

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+/* Navegacion */
+    menuBtn = document.querySelector('nav . btn')
+    menuLst = document.querySelector('nav . menu')
+
+    menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
